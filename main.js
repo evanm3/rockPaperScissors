502561872035fa74258e29916d432e71c7d3c0d9
evanm3 <132603114+evanm3@users.noreply.github.com>
2023-08-04 16:06:58 -0600

    add round within match function

--- main.js
+++ main.js
@@ -1,45 +1,70 @@
-// console.log("Butterfinger BBs");
-
 const optionsArray = ['rock', 'paper', 'scissors'];
+
+let playerScore = 0;
+let cpuScore = 0;
+
+function game(){
+    for (let i = 1; i <= 10; i++) {
+    roundWithinMatch();
+    if (playerScore === 3) {
+        alert(`You won! CPU: ${cpuScore} to PLAYER: ${playerScore}`);
+        break;
+    }else if (cpuScore === 3) {
+        alert(`You lost. CPU: ${cpuScore} to PLAYER: ${playerScore}`);
+        break;
+    }
+    }
+}
+
+function roundWithinMatch(){
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
+    console.log(`PLAYER CHOICE: ${playerSelection} CPU CHOICE: ${computerSelection}`)
+    console.log(singleRound(playerSelection, computerSelection));
+    console.log(`player: ${playerScore}, cpu: ${cpuScore}`);
+
+}
+
+function singleRound(playerSelection, computerSelection) { 
+    
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw!"); 
+        return;
+} else if(playerSelection === "rock" && computerSelection === "scissors"){
+    console.log("Rock smashes Scissors. You win!");
+    return playerScore++; 
+} else if(playerSelection === "rock" && computerSelection === "paper") {
+    console.log("Rock is covered by Paper. You lose!");
+    return cpuScore++; 
+} else if(playerSelection === "paper" && computerSelection === "rock") {
+    console.log("Paper covers Rock. You win!");
+    return playerScore++; 
+} else if(playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("Paper is cut by Scissors! You lose!");
+    return cpuScore++; 
+} else if(playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("Scissors cut Paper! You win!");
+    return playerScore++; 
+} else if(playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("Scissors smashed by Rock. You lose!");
+    return cpuScore++; 
+} else {
+    console.log("invalid response");
+    return false;
+}
+}
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * optionsArray.length);
     return (optionsArray[computerChoice]);
 }
 
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-const playerSelectionPrompt = prompt("Rock, Paper, or Scissors?", "R/P/S");
-const playerSelection = playerSelectionPrompt.toLowerCase();
-
-function singleRound(playerSelection, computerSelection) { 
-    if (playerSelection === computerSelection) {
-        console.log("It's a draw!");
-} else if(playerSelection === "rock" && computerSelection === "scissors"){
-    console.log("Rock smashes Scissors. You win!");
-} else if(playerSelection === "rock" && computerSelection === "paper") {
-    console.log("Rock is covered by Paper. You lose!")
-} else if(playerSelection === "paper" && computerSelection === "rock") {
-    console.log("Paper covers Rock. You win!")
-} else if(playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("Paper is cut by Scissors! You lose!")
-} else if(playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("Scissors cut Paper! You win!")
-} else if(playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("Scissors smashed by Rock. You lose!")
-} else {
-    console.log("incorrect response")
-}
+function getPlayerChoice() {
+    let playerSelectionPrompt = prompt("Rock, Paper, or Scissors?", "Rock/Paper/Scissors");
+    while (playerSelectionPrompt == null) {
+        playerSelectionPrompt = prompt("Rock, Paper, or Scissors?", "Rock/Paper/Scissors");
+    }
+    return (playerSelectionPrompt.toLowerCase())
 }
 
-
-console.log(singleRound(playerSelection, computerSelection));;
-
-// console.log(playerSelection);
-// console.log(computerSelection);
-// alert(singleRound());
-
-// console.log(getComputerChoice());
-
-// let randomNumber = Math.floor(Math.random()*3);
-// console.log(randomNumber);
+console.log(game());

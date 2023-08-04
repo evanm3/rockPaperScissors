32c0cbf2eb4dd94e061fe6514af6c0718c2c9e63
evanm3 <132603114+evanm3@users.noreply.github.com>
2023-08-03 19:49:21 -0600

    add playerselection prompt and gameround function

--- main.js
+++ main.js
@@ -4,9 +4,40 @@ const optionsArray = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * optionsArray.length);
-    console.log(optionsArray[computerChoice]);
+    return (optionsArray[computerChoice]);
 }
-getComputerChoice();
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+const playerSelectionPrompt = prompt("Rock, Paper, or Scissors?", "R/P/S");
+const playerSelection = playerSelectionPrompt.toLowerCase();
+
+function singleRound(playerSelection, computerSelection) { 
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw!");
+} else if(playerSelection === "rock" && computerSelection === "scissors"){
+    console.log("Rock smashes Scissors. You win!");
+} else if(playerSelection === "rock" && computerSelection === "paper") {
+    console.log("Rock is covered by Paper. You lose!")
+} else if(playerSelection === "paper" && computerSelection === "rock") {
+    console.log("Paper covers Rock. You win!")
+} else if(playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("Paper is cut by Scissors! You lose!")
+} else if(playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("Scissors cut Paper! You win!")
+} else if(playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("Scissors smashed by Rock. You lose!")
+} else {
+    console.log("incorrect response")
+}
+}
+
+
+console.log(singleRound(playerSelection, computerSelection));;
+
+// console.log(playerSelection);
+// console.log(computerSelection);
+// alert(singleRound());
 
 // console.log(getComputerChoice());
 

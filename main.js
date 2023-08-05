90f04cb53404f61b8430e7ad3b4aa0dca1e6e097
evanm3 <132603114+evanm3@users.noreply.github.com>
2023-08-05 14:00:40 -0600

    clean code and var func names

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-const optionsArray = ['rock', 'paper', 'scissors'];
+// an unchanging variable can be in all caps instead of camelCase
+const OPTIONS = ['rock', 'paper', 'scissors'];
 
+
+// let must be used because the score will be changing
 let playerScore = 0;
 let cpuScore = 0;
 
+// function loops to 10 to likely reach a best 3 out of 5 endpoint 
 function game(){
     for (let i = 1; i <= 10; i++) {
-    roundWithinMatch();
-    if (playerScore === 3) {
-        alert(`You won! CPU: ${cpuScore} to PLAYER: ${playerScore}`);
-        break;
-    }else if (cpuScore === 3) {
-        alert(`You lost. CPU: ${cpuScore} to PLAYER: ${playerScore}`);
-        break;
-    }
+        singleRoundResults();
+            if (playerScore === 3) {
+                alert(`You won! CPU: ${cpuScore} to PLAYER: ${playerScore}`);
+                break;
+            }else if (cpuScore === 3) {
+                alert(`You lost. CPU: ${cpuScore} to PLAYER: ${playerScore}`);
+                break;
+            }
     }
 }
 
-function roundWithinMatch(){
+function singleRoundResults(){
     const computerSelection = getComputerChoice();
     const playerSelection = getPlayerChoice();
     console.log(`PLAYER CHOICE: ${playerSelection} CPU CHOICE: ${computerSelection}`)
@@ -30,33 +34,33 @@ function singleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("It's a draw!"); 
         return;
-} else if(playerSelection === "rock" && computerSelection === "scissors"){
-    console.log("Rock smashes Scissors. You win!");
-    return playerScore++; 
-} else if(playerSelection === "rock" && computerSelection === "paper") {
-    console.log("Rock is covered by Paper. You lose!");
-    return cpuScore++; 
-} else if(playerSelection === "paper" && computerSelection === "rock") {
-    console.log("Paper covers Rock. You win!");
-    return playerScore++; 
-} else if(playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("Paper is cut by Scissors! You lose!");
-    return cpuScore++; 
-} else if(playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("Scissors cut Paper! You win!");
-    return playerScore++; 
-} else if(playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("Scissors smashed by Rock. You lose!");
-    return cpuScore++; 
-} else {
-    console.log("invalid response");
-    return false;
-}
+    } else if(playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("Rock smashes Scissors. You win!");
+        return playerScore++; 
+    } else if(playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Rock is covered by Paper. You lose!");
+        return cpuScore++; 
+    } else if(playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Paper covers Rock. You win!");
+        return playerScore++; 
+    } else if(playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("Paper is cut by Scissors! You lose!");
+        return cpuScore++; 
+    } else if(playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("Scissors cut Paper! You win!");
+        return playerScore++; 
+    } else if(playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Scissors smashed by Rock. You lose!");
+        return cpuScore++; 
+    } else {
+        console.log("invalid response");
+        return false;
+    }
 }
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * optionsArray.length);
-    return (optionsArray[computerChoice]);
+    let computerChoice = Math.floor(Math.random() * OPTIONS.length);
+    return (OPTIONS[computerChoice]);
 }
 
 function getPlayerChoice() {
